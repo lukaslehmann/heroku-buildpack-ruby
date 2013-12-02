9fa91523d7cd9765eb6728234c7f9378ff9ed464
lukaslehmann <lukas92lehmann@gmail.com>
2013-12-02 15:22:05 -0800

    lol syntax error

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -413,7 +413,7 @@ WARNING
   # runs bundler to install the dependencies
   def build_bundler
     #install unix odbc
-    run(sudo apt-get install unixodbc-dev)
+    run("sudo apt-get install unixodbc-dev")
     log("bundle") do
       bundle_without = ENV["BUNDLE_WITHOUT"] || "development:test"
       bundle_bin     = "bundle"
